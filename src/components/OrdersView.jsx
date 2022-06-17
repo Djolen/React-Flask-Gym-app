2043f754a29e71f8b4b9d9e501a8d963b0a83f12
Djolen <djordjert5419@gs.viser.edu.rs>
2022-06-18 01:16:40 +0200

    css

--- src/components/OrdersView.jsx
+++ src/components/OrdersView.jsx
@@ -17,10 +17,7 @@ const OrdersView = () => {
     },[])
 
     const handleContact = (datakey) =>{
-        /* 
-            console.log("entered handlecontact")
-            console.log(datakey)
-        */
+
         axios.post("/getcontact",{
              datakey:datakey,
         }).then((res)=>{
@@ -30,7 +27,6 @@ const OrdersView = () => {
     }
 
     const handleDelete = (datakey) =>{
-        /* console.log("delete") */
 
         axios.post("/deleteorder",{
             datakey:datakey,
@@ -41,33 +37,37 @@ const OrdersView = () => {
     }
 
     return (
-        <div className='container mx-auto my-6 relative min-h-[100vh] flex flex-col items-center '>
+        <div className='bg-red-500  container mx-auto my-6 pt-6 relative min-h-[100vh] flex flex-col items-center '>
 
-            <div className='hidden mb-4 lg:flex '> 
-                <p className='text-4xl text-bold p-6 bg-white '      > Name </p> 
-                <p className='text-4xl text-bold p-6 bg-slate-600 '  > Surname </p> 
-                <p className='text-4xl text-bold p-6 bg-white '      > Age </p> 
-                <p className='text-4xl text-bold p-6 bg-slate-600'   > Weight </p> 
-                <p className='text-4xl text-bold p-6 bg-white '      > Diet Plan </p> 
-                <p className='text-4xl text-bold p-6 bg-slate-600 '  > Training Plan </p> 
-                <p className='text-4xl text-bold p-6 bg-white '      > Consultation </p>
-            </div>
+        <div className='flex flex-col container bg-slate-500 p-2'>
 
+             <div className='w-[100%] flex items-center justify-center flex-col mb-4 md:flex-row '> 
+                <p className='text-md xl:text-2xl text-bold p-6 bg-white w-[100%] md:w-[14.3%] '      > Name </p> 
+                <p className='text-md xl:text-2xl text-bold p-6 bg-slate-600 w-[100%] md:w-[14.3%] '  > Surname </p> 
+                <p className='text-md xl:text-2xl text-bold p-6 bg-white w-[100%] md:w-[14.3%] '      > Age </p> 
+                <p className='text-md xl:text-2xl text-bold p-6 bg-slate-600 w-[100%] md:w-[14.3%]'   > Weight </p> 
+                <p className='text-md xl:text-2xl text-bold p-6 bg-white w-[100%] md:w-[14.3%] '      > Diet Plan </p> 
+                <p className='text-md xl:text-2xl text-bold p-6 bg-slate-600 w-[100%] md:w-[14.3%]'   > TrainingPlan </p> 
+                <p className='text-md xl:text-2xl text-bold p-6 bg-white w-[100%] md:w-[14.3%] '      > Consultation </p> 
+            </div> 
 
+            
             {data.map((order,i)=>{
-                return  <div  key={i + (Math.random() * 5000)} className='flex min-w-[250px] flex-col mb-4 md:flex-row '> 
-                            <p className='text-4xl text-bold p-6 bg-white '     key={i + (Math.random() * 5000)} > {order[1]} </p> 
-                            <p className='text-4xl text-bold p-6 bg-slate-600 ' key={i + (Math.random() * 5000)} > {order[2]} </p> 
-                            <p className='text-4xl text-bold p-6 bg-white '     key={i + (Math.random() * 5000)} > {order[3]} </p> 
-                            <p className='text-4xl text-bold p-6 bg-slate-600'  key={i + (Math.random() * 5000)} > {order[4]} </p> 
-                            <p className='text-4xl text-bold p-6 bg-white '     key={i + (Math.random() * 5000)} > {order[5]} </p> 
-                            <p className='text-4xl text-bold p-6 bg-slate-600 ' key={i + (Math.random() * 5000)} > {order[6]} </p> 
-                            <p className='text-4xl text-bold p-6 bg-white '     key={i + (Math.random() * 5000)} > {order[7]} </p>
-                            <p onClick={() =>handleContact(order[0])}  key={i + (Math.random() * 5000)} className='viewButton text-4xl text-bold p-6 bg-white text-teal-700' > CONTACT </p>
-                            <p onClick={() =>handleDelete(order[0])}   key={i + (Math.random() * 5000)} className='viewButton text-4xl text-bold p-6 bg-white text-red-600' > DELETE </p> 
+                return  <div  key={i + (Math.random() * 5000)} className=' w-[100%] flex items-center justify-center flex-col mb-4 md:flex-row '> 
+                            <p className='text-lg xl:text-2xl text-bold p-6 bg-white w-[100%] md:w-[11.1%]'     key={i + (Math.random() * 5000)} > {order[1]} </p> 
+                            <p className='text-lg xl:text-2xl text-bold p-6 bg-slate-600 w-[100%] md:w-[11.1%]' key={i + (Math.random() * 5000)} > {order[2]} </p> 
+                            <p className='text-lg xl:text-2xl text-bold p-6 bg-white w-[100%] md:w-[11.1%]'     key={i + (Math.random() * 5000)} > {order[3]} </p> 
+                            <p className='text-lg xl:text-2xl text-bold p-6 bg-slate-600 w-[100%] md:w-[11.1%]'  key={i + (Math.random() * 5000)} > {order[4]} </p> 
+                            <p className='text-lg xl:text-2xl text-bold p-6 bg-white w-[100%] md:w-[11.1%]'     key={i + (Math.random() * 5000)} > {order[5]} </p> 
+                            <p className='text-lg xl:text-2xl text-bold p-6 bg-slate-600 w-[100%] md:w-[11.1%]' key={i + (Math.random() * 5000)} > {order[6]} </p> 
+                            <p className='text-lg xl:text-2xl text-bold p-6 bg-white w-[100%] md:w-[11.1%]'     key={i + (Math.random() * 5000)} > {order[7]} </p>
+                            <p className='w-[100%] md:max-w-[11.1%] viewButton text-lg xl:text-2xl text-bold p-6 bg-white text-teal-700' onClick={() => handleContact(order[0])}  key={i + (Math.random() * 5000)}  > CONTACT </p>
+                            <p className='w-[100%] md:max-w-[11.1%] viewButton text-lg xl:text-2xl text-bold p-6 bg-white text-red-600' onClick={() => handleDelete(order[0])}   key={i + (Math.random() * 5000)}  > DELETE </p> 
                         </div>
             })}
-        </div>
+            </div>
+            </div>
+
     )
 }
 
